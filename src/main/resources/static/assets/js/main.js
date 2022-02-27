ca068d6a20acf0acd719f966298785039e2bde6c
KimMinNyeon <alssus123@innerwave.co.kr>
2022-02-27 18:37:12 +0900

    1차

---
+++ src/main/resources/static/assets/js/main.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+    $(".about-direction").click(function(event){
+        let direction = event.target.id;
+        if(direction == "right"){
+            $("div#up > .caracter").eq(0).appendTo('div#up');
+            $("div#down > .caracter").eq(0).appendTo('div#down');
+            
+        }
+        if(direction == "left"){
+            $("div#up > .caracter").eq(5).prependTo('div#up');
+            $("div#down > .caracter").eq(5).prependTo('div#down');
+            
+        }
+      })
+});
+function videoModal (category) {
+    $("#videoModal").modal({
+        fadeDuration: 500
+      });
+    $(".categoryVideo").html(category);
+    $("iframe").attr('src', category);
+    // $("iframe").attr('width', '100%');
+    // $("iframe").attr('height', '40%');
+}
+
+function serviceModal () {
+    $("#serviceModal").modal({
+        fadeDuration: 500
+      });
+}
+
+
